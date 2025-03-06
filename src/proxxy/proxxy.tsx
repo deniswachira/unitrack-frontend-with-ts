673d26b05030b6988ecdc36bcb3ee200b65662ac
Denis Wachira <deniswachira77@gmail.com>
2025-03-06 23:38:08 +0300

    Update API domain to production URL and comment out local development URL

--- src/proxxy/proxxy.tsx
+++ src/proxxy/proxxy.tsx
@@ -1,3 +1,3 @@
-// export const apiDomain = 'https://unitrack-backend-afedc0adenazgfht.eastus-01.azurewebsites.net/';
-export const apiDomain = 'http://localhost:5001/';
+export const apiDomain = 'https://unitrack-backend-afedc0adenazgfht.eastus-01.azurewebsites.net/';
+// export const apiDomain = 'http://localhost:5001/';
 export const modelApiDomain = 'https://recommendationmodel-fbarbzdsczhqhphb.southafricanorth-01.azurewebsites.net/predict_career'
